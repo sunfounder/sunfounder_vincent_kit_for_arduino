ca829799604322d41a686db27937e269e4aeee5f
xiemeiping <1035791926@qq.com>
2023-09-22 11:17:38 +0800

    change the code

--- code/3.1_reversingAid/3.1_reversingAid.ino
+++ code/3.1_reversingAid/3.1_reversingAid.ino
@@ -1,79 +1,100 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+// Define pins for the ultrasonic sensor
 const int ledPin = 8;
 const int echoPin = 4;
 const int trigPin = 5;
 const int buzzerPin = 9;
-const long intervalLcd = 1000;
-long intervalAlert = -1;
 
-unsigned long previousMillisL = 0;
-unsigned long previousMillisA = 0;
+// Initialize the intervals variable
+unsigned long intervals = 1000;
+unsigned long previousMillis = 0;
 
+// Initialize the distance variable
+float distance = 0;
+
+// Initialize the LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
+  // Begin serial communication at 115200 baud rate
+  Serial.begin(115200);
+
+  // Set echoPin as input and trigPin as output
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
+
+  // Initialize the buzzer pin
+  pinMode(buzzerPin, OUTPUT);
+
+  // Initialize the led pin
+  pinMode(ledPin, OUTPUT);
+
+  // Start the LCD
+  lcd.clear();
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  float distanceValue = readSensorData();
-  unsigned long currentMillisL = millis();
-  if (currentMillisL - previousMillisL >= intervalLcd)
-  {
-    previousMillisL = currentMillisL;
-    lcdPrint(distanceValue);
+  // Update the distance
+  distance = readDistance();
+
+  // Update intervals based on distance
+  if (distance <= 10) {
+    intervals = 300;
+  } else if (distance <= 20) {
+    intervals = 500;
+  } else if (distance <= 50) {
+    intervals = 1000;
+  } else {
+    intervals = 2000;
   }
-  distanceJudgment(distanceValue);
-  if (intervalAlert != -1)
-  {
-    unsigned long currentMillisA = millis();
-    if (currentMillisA - previousMillisA >= intervalAlert)
-    {
-      previousMillisA = currentMillisA;
-      alertWork();
-    }
+
+  // Check if it's time to beep
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= intervals) {
+    Serial.println("Beeping!");
+    beep();
+    previousMillis = currentMillis;
   }
+
+  // Update the distance on the LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dis: ");
+  lcd.print(distance);
+  lcd.print(" cm");
+
+  delay(100);
 }
 
-void alertWork() {
+// Beep function for the buzzer
+void beep() {
   digitalWrite(ledPin, HIGH);
   tone(9, 440, 100);
   delay(100);
   digitalWrite(ledPin, LOW);
 }
 
-void lcdPrint(float distance) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("distance:");
-  lcd.setCursor(9, 0);
-  lcd.print(distance);
-}
-
-float readSensorData(void) {
+// Function to read data from the ultrasonic sensor
+float readDistance() {
+  // Trigger a low signal before sending a high signal
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+  // Send a 10-microsecond high signal to the trigPin
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
+  // Return to low signal
   digitalWrite(trigPin, LOW);
-  float distance = pulseIn(echoPin, HIGH)/58.00;
-  return  distance;
-}
 
-void distanceJudgment(float distance) {
-  if (distance <= 25) {
-    intervalAlert = 300;
-  }
-  else if (distance <= 50) {
-    intervalAlert = 800;
-  }
-  else {
-    intervalAlert = -1;
-  }
+  // Measure the duration of the high signal on the echoPin
+  unsigned long microsecond = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance using the speed of sound (29.00µs per centimeter)
+  float distance = microsecond / 29.00 / 2;
+
+  // Return the calculated distance
+  return distance;
 }
