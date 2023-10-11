0d2cda2a523d471f205424d6db49032235802321
k <142291667+Serendipity-kk@users.noreply.github.com>
2023-10-11 11:22:01 +0800

    Add language link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,4 +99,12 @@ rst_epilog = """
 
     <a href="https://www.arduino.cc/en/software#future-version-of-the-arduino-ide" target="_blank">Arduino IDE 2.0.0 Page</a>
 
+.. |link_jp_tutorials| raw:: html
+
+    <a href="https://docs.sunfounder.com/projects/vincent-kit-de/de/latest/" target="_blank">日本語オンライン教材</a>
+
+.. |link_en_tutorials| raw:: html
+
+    <a href="https://docs.sunfounder.com/projects/vincent-kit/en/latest/" target="_blank">English Online-tutorials</a>
+
 """
