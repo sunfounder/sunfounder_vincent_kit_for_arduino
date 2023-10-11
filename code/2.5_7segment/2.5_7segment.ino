7c0bccb3724260c4b9eb6f16a76369ca34d02e56
Wulu <wulu@wulu.zone>
2023-10-11 11:25:40 +0800

    Update 2.5_7segment.ino

--- code/2.5_7segment/2.5_7segment.ino
+++ code/2.5_7segment/2.5_7segment.ino
@@ -1,182 +1,190 @@
-const int a=7; //a of 7-segment attach to digital pin 7
-const int b=6; //b of 7-segment attach to digital pin 6
-const int c=5; //c of 7-segment attach to digital pin 5
-const int d=11;//d of 7-segment attach to digital pin 11
-const int e=10;//e of 7-segment attach to digital pin 10
-const int f=8;//f of 7-segment attach to digital pin 8
-const int g=9;//g of 7-segment attach to digital pin 9
-const int dp=4;//dp of 7-segment attach to digital pin 4
+// Define pins for each segment of the 7-segment display
+const int a = 7;
+const int b = 6;
+const int c = 5;
+const int d = 11;
+const int e = 10;
+const int f = 8;
+const int g = 9;
+const int dp = 4;  // Decimal point
 
-void setup()
-{
-  //loop over thisPin from 4 to 11 and set them all to output
-  for(int thisPin = 4;thisPin <= 11;thisPin++)
-  {
-    pinMode(thisPin,OUTPUT);
+void setup() {
+  // Set pins 4 to 11 as output
+  for (int thisPin = 4; thisPin <= 11; thisPin++) {
+    pinMode(thisPin, OUTPUT);
   }
-  digitalWrite(dp,LOW);//turn the dp of the 7-segment off 
+  // Initialize display by turning off all segments
+  turnOffAllSegments();
 }
 
-void loop()
-{
-  digital_1();//diaplay 1 to the 7-segment
-  delay(1000);//wait for a second
-  digital_2();//diaplay 2 to the 7-segment
-  delay(1000); //wait for a second
-  digital_3();//diaplay 3 to the 7-segment
-  delay(1000); //wait for a second
-  digital_4();//diaplay 4 to the 7-segment
-  delay(1000); //wait for a second
-  digital_5();//diaplay 5 to the 7-segment
-  delay(1000); //wait for a second
-  digital_6();//diaplay 6 to the 7-segment
-  delay(1000); //wait for a second
-  digital_7();//diaplay 7 to the 7-segment
-  delay(1000); //wait for a second
-  digital_8();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_9();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_A();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_b();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_C();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_d();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_E();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
-  digital_F();//diaplay 8 to the 7-segment
-  delay(1000); //wait for a second
+void loop() {
+  // Display numbers 1 to 9 and letters A to F sequentially
+  digital_1(); delay(1000);
+  digital_2(); delay(1000);
+  digital_3(); delay(1000);
+  digital_4(); delay(1000);
+  digital_5(); delay(1000);
+  digital_6(); delay(1000);
+  digital_7(); delay(1000);
+  digital_8(); delay(1000);
+  digital_9(); delay(1000);
+  digital_A(); delay(1000);
+  digital_b(); delay(1000);
+  digital_C(); delay(1000);
+  digital_d(); delay(1000);
+  digital_E(); delay(1000);
+  digital_F(); delay(1000);
 }
 
-void digital_1(void) //diaplay 1 to the 7-segment
+void digital_1()  //diaplay 1 to the 7-segment
 {
-  digitalWrite(c,HIGH);//turn the c of the 7-segment on
-  digitalWrite(b,HIGH);//turn the b of the 7-segment on
-  for(int j = 7;j <= 11;j++)//turn off the others
-    digitalWrite(j,LOW);
+  turnOffAllSegments();
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
 }
-void digital_2(void) //diaplay 2 to the 7-segment
+
+void digital_2()  //diaplay 2 to the 7-segment
 {
-  digitalWrite(b,HIGH);
-  digitalWrite(a,HIGH);
-  for(int j = 9;j <= 11;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(c,LOW);
-  digitalWrite(f,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(d, HIGH);
 }
-void digital_3(void) //diaplay 3 to the 7-segment
+
+void digital_3()  //diaplay 3 to the 7-segment
 {
-  unsigned char j;
-  digitalWrite(g,HIGH);
-  digitalWrite(d,HIGH);
-  for(j=5;j<=7;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(f,LOW);
-  digitalWrite(e,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_4(void) //diaplay 4 to the 7-segment
+void digital_4()  //diaplay 4 to the 7-segment
 {
-  digitalWrite(c,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
-  digitalWrite(a,LOW);
-  digitalWrite(e,LOW);
-  digitalWrite(d,LOW);
+  turnOffAllSegments();
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_5(void) //diaplay 5 to the 7-segment
+
+void digital_5()  //diaplay 5 to the 7-segment
 {
-  unsigned char j;
-  for(j = 7;j <= 9;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(b,LOW);
-  digitalWrite(e,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
 }
-void digital_6(void) //diaplay 6 to the 7-segment
+
+void digital_6()  //diaplay 6 to the 7-segment
 {
-  unsigned char j;
-  for(j = 7;j <= 11;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(b,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_7(void) //diaplay 7 to the 7-segment
+
+void digital_7()  //diaplay 7 to the 7-segment
 {
-  unsigned char j;
-  for(j = 5;j <= 7;j++)
-    digitalWrite(j,HIGH);
-  for(j = 8;j <= 11;j++)
-    digitalWrite(j,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
 }
-void digital_8(void) //diaplay 8 to the 7-segment
+
+void digital_8()  //diaplay 8 to the 7-segment
 {
-  unsigned char j;
-  for(j = 5;j <=11;j++)
-    digitalWrite(j,HIGH);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_9(void) //diaplay 9 to the 7-segment
+
+void digital_9()  //diaplay 9 to the 7-segment
 {
-  unsigned char j;
-  for(j = 5;j <=9;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_A(void) //diaplay A to the 7-segment
+
+void digital_A()  //diaplay A to the 7-segment
 {
-  unsigned char j;
-  for(j = 5;j <=10;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(d,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_b(void) //diaplay b to the 7-segment
+
+void digital_b()  //diaplay b to the 7-segment
 {
-  unsigned char j;
-  for(j = 7;j <=11;j++)
-    digitalWrite(j,HIGH);
-  digitalWrite(a,LOW);
-  digitalWrite(b,LOW);
+  turnOffAllSegments();
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_C(void) //diaplay C to the 7-segment
+
+void digital_C()  //diaplay C to the 7-segment
 {
-  digitalWrite(a,HIGH);
-  digitalWrite(b,LOW);
-  digitalWrite(c,LOW);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,HIGH);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,LOW);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(d, HIGH);
 }
-void digital_d(void) //diaplay d to the 7-segment
+
+void digital_d()  //diaplay d to the 7-segment
 {
-  unsigned char j;
-  digitalWrite(a,LOW);
-  digitalWrite(f,LOW);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(j,HIGH);
-  for(j = 9;j <=11;j++)
-    digitalWrite(j,HIGH);
+  turnOffAllSegments();
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_E(void) //diaplay E to the 7-segment
+
+void digital_E()  //diaplay E to the 7-segment
 {
-  unsigned char j;
-  digitalWrite(b,LOW);
-  digitalWrite(c,LOW);
-  for(j = 7;j <=11;j++)
-    digitalWrite(j,HIGH);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
-void digital_F(void) //diaplay F to the 7-segment
+
+void digital_F()  //diaplay F to the 7-segment
 {
-  unsigned char j;
-  digitalWrite(b,LOW);
-  digitalWrite(c,LOW);
-  digitalWrite(d,LOW);
-  for(j = 7;j <=10;j++)
-    digitalWrite(j,HIGH);
+  turnOffAllSegments();
+  digitalWrite(a, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+
+void turnOffAllSegments() {
+  // Turn off all segments of the 7-segment display
+  for (int thisPin = 4; thisPin <= 11; thisPin++) {
+    digitalWrite(thisPin, LOW);
+  }
 }
