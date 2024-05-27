3002ce85f317079470a61964c2182ed2e7e61b77
harumiume <127194455+harumiume@users.noreply.github.com>
2024-05-27 09:44:14 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ rst_epilog = """
 
 .. |link_sf_facebook| raw:: html
 
-    <a href="https://bit.ly/3WMiqZg" target="_blank">hier</a>
+    <a href="https://bit.ly/raphaelkit" target="_blank">hier</a>
 
 .. |link_download_arduino| raw:: html
 
